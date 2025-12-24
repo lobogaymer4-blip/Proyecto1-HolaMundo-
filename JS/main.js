ca38c873be69f5dadf0e133b8b752ccc073fd0fe
lobogaymer4-blip <lobogaymer4@gmail.com>
2025-12-24 18:27:57 -0300

    WIP: añade sección 'qué es CSS' y contenidp inicial

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 function cambiarMensaje() {
   const el = document.getElementById("mensaje-texto");
   if (!el) return;
-  
+
   // Alterna entre dos mensajes
   if (el.classList.contains('recuadro')) {
     el.textContent = "Este es el mensaje original.";
